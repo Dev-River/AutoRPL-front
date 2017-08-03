bfacd79970cc5ed180e6c612c9fcc6a2101d1a3f
yellow_river <river_yellow@163.com>
2017-08-03 17:27:29 +0800

    2017-08-03

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,6 +16,21 @@ Page({
     })
   },
   onLoad: function () {
-    console.log('onLoad')
+    console.log('onLoad');
+    if (app.globalData.userInfo) {
+        this.setData({
+            userInfo: app.globalData.userInfo,
+            hasUserInfo: true
+        })
+    } else {
+        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+        // 所以此处加入 callback 以防止这种情况
+        app.userInfoReadyCallback = res => {
+            this.setData({
+                userInfo: res.userInfo,
+                hasUserInfo: true
+            })
+        }
+    }
   }
 })
